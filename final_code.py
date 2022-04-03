6145cbe8dbb35f246ba78eae2f82b60ac203eee8
JensGeukens <101265203+JensGeukens@users.noreply.github.com>
2022-04-03 17:50:36 +0200

    Add files via upload

--- final_code.py
+++ final_code.py
@@ -4,7 +4,7 @@ import time                 #library needed to wait
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 1000000  
+spi.max_speed_hz = 1000000
 sensor1 = False             #both motion sensors are initially set to False
 sensor2 = False
 
@@ -24,8 +24,8 @@ mydb = mysql.connector.connect(         #setup connection to sql database
 if __name__ == "__main__":
     while(True):
         v1 = (readChannel(0)) / 1023.0 * 3.3    #converting value of the ADC to a voltage
-        v2 = (readChannel(1)) / 1023.0 * 5
-        v3 = (readChannel(3)) / 1023.0 * 5
+        v2 = (readChannel(1)) / 1023.0 * 3.3
+        v3 = (readChannel(3)) / 1023.0 * 3.3
 
 
         if(v2<0.5):                             #first sensor is triggered
@@ -34,25 +34,25 @@ if __name__ == "__main__":
                 mycursor = mydb.cursor()
                 sql = "UPDATE people SET number = number - 1 WHERE id= 1"       #update database with -1 people
                 mycursor.execute(sql)
-                mydb()
+                mydb.commit()
                 sensor1 = False                 #set both sensors back to False
                 sensor2 = False
 
 
         if(v3<0.5):                             #second sensor is triggered
             sensor2 = True
-            if (sensor2==True):                 #first sensor is also triggered (person entering)
+            if (sensor1==True):                 #first sensor is also triggered (person entering)
                 mycursor = mydb.cursor()
                 sql = "UPDATE people SET number = number + 1 WHERE id= 1"       #update database with +1 people
                 mycursor.execute(sql)
                 mydb.commit()
-                sensor1 =False                  #set both sensors back to False
+                sensor1 = False                  #set both sensors back to False
                 sensor2 = False
 
 
 
-        distance = 151.54*v1**4-526.84*v1**3+656.19*v1**2-367.48*v1+107.96        #convert voltage of distance sensor to distance(cm)
-        amount = distance/8                                                        #amount of bottles that equals to the distance
+        distance = 28.387*v1**4-155.6*v1**3+314.59*v1**2-293.67*v1+128.06        #convert voltage of distance sensor to distance(cm)
+        amount = (40/6) - (distance/6)                                           #amount of bottles with diameter 6cm in a rack of 25cm at a distance of 15cm
         mycursor = mydb.cursor()
         sql = "UPDATE winestock SET number ={number} WHERE idstock = {idstock}".format(number = amount, idstock =1)    #update database with current amount of bottles
         mycursor.execute(sql)
